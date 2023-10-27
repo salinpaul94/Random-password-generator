bc78f31d5f9608982680f82dcc385c72304c3a1f
salinpaul valooran <salinpv@hotmail.com>
2023-10-28 02:34:29 +0530

    constants for generating password

---
+++ version_01/index.js
@@ -0,0 +1,7 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "SALINPAUL";
+const lowerCase = "valooran";
+const number = "1234";
+const symbols = "!@#";
